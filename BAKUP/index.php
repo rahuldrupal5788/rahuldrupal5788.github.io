45b5d2b1cb42f6677476a82da94ce2c47c2775f2
Rahul Kumar <rkumar@VELOCIS.IN>
2020-07-31 14:54:30 +0530

    a

---
+++ BAKUP/index.php
@@ -0,0 +1,28 @@
+<!-- <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+<div class="elfsight-app-e53d876e-e277-4f80-8f9c-d4c2e630eb29"></div> -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Gupshup what's api integration</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">  
+    <link rel="stylesheet" type="text/css" href="css/style.css">  
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Muli&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
+  </head>
+  <body>
+    <div class="bgimg">
+      <!-- <div class="topleft"><p>Logo</p></div> -->
+      <div class="middle">
+        <h1>Gupshup What's up API Interation</h1>
+        <!-- <hr>
+        <p>35 days left</p> -->
+      </div>
+      <div class="bottomleft"><p>Smart mechanisms to get active and opted in users</p></div>
+    </div>
+  </body>
+</html>
+
+<!-- This is Gupshup what's up api script -->
+<script async src='https://www.buildquickbots.com/gsui/js/embedScript/gs_wa_widget.js' data-appid='5dd529a0-2706-4f6a-9c04-2abb48457134' data-appname='VelocisDemoAccessAPI' data-source='WEB' data-env='PROD' data-lang='en_US'></script>
